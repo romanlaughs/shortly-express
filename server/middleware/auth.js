af4103af291a1ec8d6d8ba92ba698930a80f6512
Matt Alexander <dmalexander6@gmail.com>
2022-08-18 22:59:58 -0600

    complete thursday pairing session

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,27 +3,37 @@ const Promise = require('bluebird');
 const parseCookies = require('./cookieParser');
 
 module.exports.createSession = (req, res, next) => {
-  console.log('REQ.COOKIES: ', req.cookies);
-  if (Object.keys(req.cookies).length === 0) {
+  if (!req.cookies.shortlyid) {
     return models.Sessions.create()
       .then(createdData => {
-        console.log('CREATED DATA: ', createdData);
         return models.Sessions.get({'id': createdData.insertId});
       })
       .then(gottenData => {
         req.session = {hash: gottenData.hash};
-        console.log('REQ SESSION: ', req.session);
         req.headers.cookie = res.cookie('shortlyid', gottenData.hash);
-        console.log('REQ HEADERS COOKIE: ', req.headers.cookie);
+        next();
+      })
+      .catch(err => {
+        throw err;
+      });
+  } else {
+    return models.Sessions.get({'hash': req.cookies.shortlyid})
+      .then(gottenData => {
+        req.session = {hash: gottenData.hash};
+        if (gottenData.userId) {
+          req.session.userId = gottenData.userId;
+          req.session.user = {};
+          req.session.user.username = gottenData.user.username;
+        }
+        req.headers.cookie = res.cookie('shortlyid', gottenData.hash);
         next();
       })
       .catch(err => {
         throw err;
       });
   }
-
-
 };
+
 
 /************************************************************/
 // Add additional authentication middleware functions below
