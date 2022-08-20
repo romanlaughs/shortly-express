b1360e6e8d5c1728e355c26d9898619a1cb28b5c
Nick Cassano <romanlaughs@gmail.com>
2022-08-20 14:04:48 -0700

    Next Step

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,6 +3,7 @@ const Promise = require('bluebird');
 const parseCookies = require('./cookieParser');
 
 module.exports.createSession = (req, res, next) => {
+  // if no shortly cookie
   if (!req.cookies.shortlyid) {
     return models.Sessions.create()
       .then(createdData => {
@@ -16,17 +17,32 @@ module.exports.createSession = (req, res, next) => {
       .catch(err => {
         throw err;
       });
+  // else there is a shortly cookie
   } else {
     return models.Sessions.get({'hash': req.cookies.shortlyid})
       .then(gottenData => {
-        req.session = {hash: gottenData.hash};
-        if (gottenData.userId) {
-          req.session.userId = gottenData.userId;
-          req.session.user = {};
-          req.session.user.username = gottenData.user.username;
+        if (gottenData) {
+
+          req.session = {hash: gottenData.hash};
+          // if session specifies user ID, add user data
+          if (gottenData.userId) {
+            req.session.userId = gottenData.userId;
+            req.session.user = {};
+            req.session.user.username = gottenData.user.username;
+          }
+          req.headers.cookie = res.cookie('shortlyid', gottenData.hash);
+          next();
+        } else {
+          return models.Sessions.create()
+            .then(createdData => {
+              return models.Sessions.get({'id': createdData.insertId});
+            })
+            .then(gottenData => {
+              req.session = {hash: gottenData.hash};
+              req.headers.cookie = res.cookie('shortlyid', gottenData.hash);
+              next();
+            });
         }
-        req.headers.cookie = res.cookie('shortlyid', gottenData.hash);
-        next();
       })
       .catch(err => {
         throw err;
